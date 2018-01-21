57580da8615d8caef99b64e16b95bff6e4e013c8
Ben Guo <ben@venmo.com>
2018-01-21 00:43:45 -0500

    add methods for 2 week edition

--- theater.rb
+++ theater.rb
@@ -29,10 +29,41 @@ class Theater
       f.title
     }.each_slice(2).to_a # create a 2-column grid
   end
+
+  def films_this_or_next_week
+    @films.select { |f|
+      f.week_overview || f.week2_overview
+    }.sort_by { |f| # alphabetize
+      f.title
+    }.each_slice(2).to_a # create a 2-column grid
+  end
+
 end
 
 class Film
   attr_accessor :week_dates
+
+  def week2_overview
+    week_dates = [0, 1, 2, 3, 4, 5, 6].map { |days|
+      Date.today + days + 7
+    }
+    spacer = '-'
+    day_strings = ['Su', 'M', 'T', 'W', 'Th', 'F', 'S']
+    overview = week_dates.map { |date|
+      if @dates.include?(date)
+        day_index = Integer(date.strftime('%w'))
+        day_strings[day_index]
+      else
+        spacer
+      end
+    }
+    # return nil if the film is not showing this week
+    if overview.uniq == [spacer]
+      nil
+    else
+      overview.join(" ")
+    end
+  end
 
   def week_overview
     week_dates = [0, 1, 2, 3, 4, 5, 6].map { |days|

602c4b742337ef3016dd944445fe4d7985780e87
Ben Guo <bg@stripe.com>
2019-06-30 13:10:26 -0700

    polish

--- generate.rb
+++ generate.rb
@@ -79,7 +79,7 @@ puts "~ combining results ~"
 @stats['end_utc'] = Time.now.utc.iso8601
 @today_string = Date.today.strftime('%e %b %Y')
 @htmls = []
-@scrapers.each do |s|
+@scrapers.sort_by(&:url_name).each do |s|
   scraper_filename = "#{@city}_#{s.url_name}.html"
   @htmls << File.read(scraper_filename)
   `rm '#{scraper_filename}'`

24c92e2ced068d99bca74cd549867e81f6e0b21f
Ben Guo <benzguo@gmail.com>
2019-07-02 23:39:59 -0700

    update cron

--- cron.rb
+++ cron.rb
@@ -6,8 +6,18 @@ result == true ? (puts "✅") : (puts "❗")
 result = system("./generate.rb -c sfbay")
 result == true ? (puts "✅") : (puts "❗")
 
+result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git config user.name \"render\")
+result == true ? (puts "✅") : (puts "❗")
+
+result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git config user.email \"benzguo@gmail.com\")
+result == true ? (puts "✅") : (puts "❗")
+
+result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git remote add origin \"git@github.com:openscrape/filmbot.git\")
+result == true ? (puts "✅") : (puts "❗")
+
+
 result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git commit -am \"render cron\"")
 result == true ? (puts "✅") : (puts "❗")
 
-result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git push")
+result = system("GIT_SSH_COMMAND='ssh -i ~/.ssh/id_rsa_filmbot' git push origin")
 result == true ? (puts "✅") : (puts "❗")
